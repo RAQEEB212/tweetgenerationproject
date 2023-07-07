55d1712fe14fc1033318ff8360c7f657d8c9527c
RAQEEB212 <saneelmemon45@gmail.com>
2023-07-07 15:43:41 +0500

    tweet generator

---
+++ src/pages/login/googlelogin.js
@@ -0,0 +1,31 @@
+import { GoogleLogin } from 'react-google-login'
+
+const clientId = "http://573694337447-kfqhh53vknfg2n7apfugcatb1sc43g0c.apps.googleusercontent.com"
+
+function LoginGoogle() {
+
+  const onSuccess =(res) => {
+    console.log("LOGIN SUCCESS! Current user: ",res.profileObj);
+  }
+
+  const onFailure = (res) =>{
+    console.log("LOGIN FAILED res: ",res);
+  }
+
+  return(
+    <div id="signInButton">
+      <GoogleLogin
+        clientId ={clientId}
+        buttonText "Login"
+        onSucess ={onSuccess}
+        onFailure ={onFailure}
+        cookiePolicy ={'single_host_origin'}
+        isSignedIn = {true}
+      />
+
+    </div>
+
+  )
+}
+
+export default LoginGoogle;
